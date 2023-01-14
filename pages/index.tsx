b81d48b846f930b08fc80633bfe99738eaa9c15e
FirstFirstFirstFirst <115007131+FirstFirstFirstFirst@users.noreply.github.com>
2023-01-15 05:21:00 +0700

    basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
-import 'tailwindcss/tailwind.css';
 
 function Home() {
   return (
-    <div className='h-screen w-screen flex flex-column justify-center'>
+    <div className='h-screen w-screen flex flex-column justify-center align-middle'>
       <div className='text-2xl text center'>Which Pokemon is Rounder?</div>
-      <div className='bg-pink-400 border rounded p-8'>Second</div> 
+      <div className='p-2'/>
+      <div className='bg-pink-400 border rounded p-8 flex justify items-center between max-w-2xl'>
+        <div className='w-16 h-16 bg-red-200'>/</div>
+        <div className='p-8'>VS</div>
+        <div className='w-16 h-16 bg-red-200'>/</div>
+      </div>
+
     </div>
   )
 }
 export default Home;
+
+
+
+
