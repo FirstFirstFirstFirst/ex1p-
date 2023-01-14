ddeae49cd8ddd310b6e7828a4ed54dfb802f4d0f
FirstFirstFirstFirst <115007131+FirstFirstFirstFirst@users.noreply.github.com>
2023-01-15 06:11:16 +0700

    success basic UI structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,9 +7,7 @@ import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className=''>
-      hello
-    </div>
+      <Component  {...pageProps}/>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,20 @@
 
-const Home = () => {
+export default function Home() {
   return (
-    <div className='h-screen w-screen flex flex-column justify-center align-middle'>
-      <div className='text-2xl text center'>Which Pokemon is Rounder?</div>
-      <div className='p-2'/>
-      <div className='bg-pink-400 border rounded p-8 flex justify items-center between max-w-2xl'>
-        <div className='w-16 h-16 bg-red-200'>/</div>
-        <div className='p-8'>VS</div>
-        <div className='w-16 h-16 bg-red-200'>/</div>
-      </div>
+      <div className='h-screen w-screen flex flex-col justify-center items-center'>
+        <div className='text-2xl text-center'>Which Pokemon is Rounder?</div>
+        <div className="p-2"/>
+        <div className="border rounded p-8 flex justify-between max-w-2xl ">
+            <div className="w-16 h-16 bg-red-200" />
+            <div className="p-8 item-center">VS</div>
+            <div className="w-16 h-16 bg-red-200" />          
+        </div>
 
-    </div>
+      </div>
+ 
+      
   )
 }
-export default Home;
 
 
 
