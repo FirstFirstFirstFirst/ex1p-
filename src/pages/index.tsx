540aeda97f9a0cbd96df94a81be5fdec9c2ebe4d
FirstFirstFirstFirst <115007131+FirstFirstFirstFirst@users.noreply.github.com>
2023-01-15 13:16:10 +0700

    add OptionForVote and tryna use useQuery

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,50 @@
 import { trpc } from '@/utils/trpc';
+import { getOptionsForVote } from '@/utils/getRandomPokemon';
+import { useEffect, useMemo, useState } from 'react';
 
 
 export default function Home() {
-  const hello = trpc.hello.useQuery({ text: "First"});
-  
-  if (!hello.data) return <div>Loading...</div>
 
+
+  const [ids, setIds] = useState<number[]>([]);
+
+  useEffect(()=> {
+    
+    const [first, second] : number[] = getOptionsForVote() ;
+    // const ids: number[] = [first, second];
+    // setIds(ids);
+    setIds([first, second]);
+    }, []);
+    const [first, second] = ids;
+
+
+  const firstPokemon = trpc.useQuery(["get-pokemon-by-id", {id: first}]);
+  console.log(firstPokemon.data);
+
+
+  
+
+
+    
+  
+  
   return (
       <div className='h-screen w-screen flex flex-col justify-center items-center'>
         <div className='text-2xl text-center'>Which Pokemon is Rounder?</div>
         <div className="p-2"/>
         <div className="border rounded p-8 flex justify-between max-w-2xl ">
-            <div className="w-16 h-16 bg-red-200" />
+            {/* <div className="w-16 h-16 bg-red-800">{randomNum[0]}</div>
             <div className="p-8 item-center">VS</div>
-            <div className="w-16 h-16 bg-red-200" />          
+            <div className="w-16 h-16 bg-red-800">{randomNum[1]}</div>   */}
+            <div className="w-16 h-16 bg-red-800">{first}</div>
+            <div className="p-8 item-center">VS</div>
+            <div className="w-16 h-16 bg-red-800">{second}</div>
+                    
         </div>
-  
-
-
-  
       </div>     
   )
 }
+
 
 
 
--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,18 +1,17 @@
 import { z } from 'zod';
+// import * as trpc from "@trpc/server"
 import { procedure, router } from '../trpc';
+import { PokemonClient } from 'pokenode-ts';
 
-export const appRouter = router({
-  hello: procedure
-    .input(
-      z.object({
-        text: z.string(),
-      }),
-    )
-    .query(({ input }) => {
-      return {
-        greeting: `hello ${input.text}`,
-      };
-    }),
+
+export const appRouter = trpc.router().query("get-pokemon-by-id", {
+  input: z.object({ id: z.number() }),
+  async resolve({ input }) {
+    const api = new PokemonClient();
+
+    const pokemon = await api.getPokemonById(input.id);
+    return pokemon;
+  }
 });
 
 // export type definition of API
